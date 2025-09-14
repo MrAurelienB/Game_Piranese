246ea0b60f9824139680b51f995887cba660db01
Aurelien Bach <aurelien.bach@hotmail.fr>
2025-09-14 16:54:31 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/*import {ComponentFactory} from './src/elm/elm.js';
+import {WindowBaseHandler} from './src/gui/gui.js';
+
+const componentFactory = new ComponentFactory();
+const windowBaseHandler = new WindowBaseHandler(componentFactory);
+window.windowBaseHandler = windowBaseHandler;*/
+
+/**
+ * 
+ * 
+ * 
+ */
